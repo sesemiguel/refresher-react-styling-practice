8e5fe01f6de99c0c8c06c9e0764cde431e940b32
Sese <sese.miguel9@gmail.com>
2020-07-26 12:26:26 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+
+    let currentDate = new Date();
+    let currentHour = currentDate.getHours();
+    let greetingText = "";
+
+    const customStyle = {
+      color: "yellow"
+    }
+
+    const greeting = (currentHour) => {
+      if (currentHour >= 0 && currentHour < 12) {
+        customStyle.color = "red"
+        greetingText = "Morning"
+      }
+      else if (currentHour >= 12 && currentHour < 18) {
+        customStyle.color = "green"
+        greetingText = "Afternoon"
+      }
+      else if (currentHour >= 18 && currentHour < 24) {
+        customStyle.color = "blue"
+        greetingText = "Evening"
+      }
+    }
+
+    return (
+      <>
+        {greeting(currentHour)}
+        <h1 className="heading" style={customStyle}>Good {greetingText}!</h1>
+      </>
+    )
+  }
 }
 
 export default App;
